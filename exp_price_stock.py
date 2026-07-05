3529cbcdaf8e1ae7880e996dbcf7e9dd81a6f72c
gan1-r <ganiramadhani11@gmail.com>
2026-07-05 12:33:11 +0700

    add: exp_price_stock dan note.txt

---
+++ exp_price_stock.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import mplfinance as mpf
+
+tickers = ["AAPL", "MSFT", "GOOGL", "TSLA"]
+df = yf.download(tickers, interval="1d", period="6mo", group_by="ticker", auto_adjust=False)
+
+# Reshape ke format long
+df = df.stack(level=0, future_stack=True).reset_index()
+df = df.rename(columns={"level_1": "Ticker"})
+
+# Set Date sebagai index dulu (wajib untuk resample)
+df = df.set_index("Date")
+
+# Resample per ticker (group_keys biar Ticker tetap ada di hasil)
+weekly = (
+    df.groupby("Ticker")
+      .resample("W-FRI")
+      .agg({
+          "Open": "first",
+          "High": "max",
+          "Low": "min",
+          "Close": "last",
+          "Volume": "sum"
+      })
+)
+
+# Optional: rapikan index jadi kolom biasa lagi
+df = weekly.reset_index()
+
+df = df[df['Ticker'] == 'GOOGL']
+print(df)
+df_candle = df.set_index("Date")[["Open", "High", "Low", "Close", "Volume"]]
+mpf.plot(df_candle, type="candle", volume=True, style="yahoo", title="GOOGL Weekly Candlestick")
